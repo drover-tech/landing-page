4a73bb6dac6074a4e9be39a5eef8627515a58290
Anthony Sukotjo <tech@drovertravel.com>
2024-09-24 01:48:37 +0800

    work please

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // Changed from 'hybrid' to 'server'
+  // Changed from 'hybrid' to 'server'
+  output: "hybrid",
+
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
-  adapter: vercel(), // Added Vercel adapter
+  adapter: vercel(),
 });
