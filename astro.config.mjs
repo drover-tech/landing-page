b70a5dafd81511b6f31cc89cd997a371ed61459f
drover-tech <your_new_email@example.com>
2024-09-23 02:22:39 -0400

    added ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: "server", // Changed from 'hybrid' to 'server'
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  adapter: vercel(), // Added Vercel adapter
 });
-
-
